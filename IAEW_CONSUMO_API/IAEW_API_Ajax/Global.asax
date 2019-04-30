e795a6a7666bc099e4e41dcb57ba48b36a168558
angetresca <42329268+angetresca@users.noreply.github.com>
2019-04-30 18:00:45 -0300

    Agrego clase 30-4-19

---
+++ IAEW_CONSUMO_API/IAEW_API_Ajax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IAEW_API_Ajax.Global" Language="C#" %>
---
+++ IAEW_CONSUMO_API/IAEW_API_Ajax/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IAEW_API_Ajax")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Telecom Argentina S.A.")]
+[assembly: AssemblyProduct("IAEW_API_Ajax")]
+[assembly: AssemblyCopyright("Copyright © Telecom Argentina S.A. 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04a3f35b-9c0c-459c-9ec7-5df44bfb10f6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
